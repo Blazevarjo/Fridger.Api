e78f8edb021ceb7b52e3f25b592eb640bd704e18
Jan Rubel <jasiekrubel@gmail.com>
2021-12-08 22:32:07 +0100

    Fix admin for user creation

--- fridger/users/admin.py
+++ fridger/users/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
@@ -8,8 +9,35 @@ from fridger.users.models import Friend
 User = get_user_model()
 
 
+class UserCreationForm(forms.ModelForm):
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("email", "username")
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
 class CustomUserAdmin(UserAdmin):
     list_display = ("email", "date_joined", "is_staff")
+    add_form = UserCreationForm
 
     fieldsets = (
         (None, {"fields": ("email", "password")}),
@@ -19,7 +47,7 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
     ordering = ("date_joined",)
-    add_fieldsets = ((None, {"fields": ("email", "username", "password")}),)
+    add_fieldsets = ((None, {"fields": ("email", "username", "password1", "password2")}),)
 
 
 admin.site.register(User, CustomUserAdmin)
